848252ad21d013dbe09826675b38dc30a6a2abb6
Eddie Pick <eddie@pick.fr>
2026-05-24 11:17:19 -0400

    chore: regenerate _version.py for current HEAD

--- src/web_forager/_version.py
+++ src/web_forager/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '3.0.1.post7'
-__version_tuple__ = version_tuple = (3, 0, 1, 'post7')
+__version__ = version = '3.0.1.post9'
+__version_tuple__ = version_tuple = (3, 0, 1, 'post9')
 
-__commit_id__ = commit_id = 'gcda53c398'
+__commit_id__ = commit_id = 'g5da99bc22'
